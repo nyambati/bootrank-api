ad7fe328c02de9692b1457392163ca925f5a1667
Thomas Nyambati <thomas.nyambati@andela.com>
2017-03-19 23:48:34 +0300

    ch(docker): add nodemon for fast reload
    Signed-off-by: Thomas Nyambati <thomas.nyambati@andela.com>

--- server/index.js
+++ server/index.js
@@ -3,14 +3,17 @@ const mongoose = require('mongoose');
 const { connect } = require('./config');
 
 connect(mongoose, process.env.DATABASE_URL);
-
-console.log("mongodb", process.env.DATABASE_URL)
-console.log("redis", process.env.REDIS_URL)
-
-// App
 const app = express();
-app.get('/', function(req, res) {
-  res.send('Hello world\n');
+
+app.get('/', (req, res) => {
+  return res
+    .status(200)
+    .json({
+      status: 'online',
+      ip: req.ip,
+      host: req.headers.host,
+      message: 'Bootrank is working well'
+    });
 });
 
 module.exports = app;
